b5ae42fdd365caa97b324cafc99f7b9fd63b2329
Azmi-84 <145701445+Azmi-84@users.noreply.github.com>
2024-07-09 14:25:26 +0000

    string methods

--- 01_Basics/05_Strings.js
+++ 01_Basics/05_Strings.js
@@ -23,8 +23,43 @@ const new_String_Data = new String('Assalamualikum, How are you?')
 
 // console.log(typeof new_String_Data);
 
-console.log(new_String_Data[14]);
-console.log(new_String_Data.__proto__);
+// console.log(new_String_Data[14]);
+// console.log(new_String_Data.__proto__);
 
-console.log(new_String_Data.length);
-console.log(new_String_Data.toUpperCase());
+// console.log(new_String_Data.length);
+// console.log(new_String_Data.toUpperCase());
+
+// console.log(new_String_Data.charAt(15));
+// console.log(new_String_Data.charAt(14));
+
+// console.log(new_String_Data.indexOf('H'));
+
+// const new_String_Data_Slice_Bysubsting = new_String_Data.substring(1, 13)
+
+// console.log(new_String_Data_Slice_Bysubsting);
+
+// the 13 no. of string is m but this is not included here.
+
+// const new_String_Data_Slice = new_String_Data.slice(-5, 2)
+
+// the interesting part here is that here we can give negative value also.
+
+// console.log(new_String_Data_Slice);
+
+// const trim_Function = "          azmi-84    "
+// console.log(trim_Function);
+// console.log(trim_Function.trim());
+
+// const url_Replace = "https://humble-space-train-r4g5xj9px9qr26r9.github.dev/"
+
+// console.log(url_Replace.replace('.dev/', '.com'))
+
+// const url_Checking_String = "https://humble-space-train-r4g5xj9px9qr26r9.github.dev/"
+
+// // checking whether a string is present in the string or not
+
+// console.log(url_Checking_String.includes("dev"))
+// console.log(url_Checking_String.includes("Azmi"))
+
+// string to array convert
+
