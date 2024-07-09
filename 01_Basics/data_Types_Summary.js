8da150585851857fe734ffa4ca5c82ec4ece6b8c
Azmi-84 <145701445+Azmi-84@users.noreply.github.com>
2024-07-09 12:12:45 +0000

    stack and heap memory

--- 01_Basics/data_Types_Summary.js
+++ 01_Basics/data_Types_Summary.js
@@ -13,7 +13,7 @@ const user_Name = undefined
 const id = Symbol("123")
 const another_Id = Symbol("123")
 
-console.log(id === another_Id);
+// console.log(id === another_Id);
 
 const big_Number = 98487364111111111122222222222222222222222224657777777777777474665746575647873261n
 
@@ -32,15 +32,41 @@ const my_Funtion = function(){
     console.log("Assalamualikum");
 }
 
-console.log( typeof score);
-console.log( typeof user_Email);
-console.log( typeof num);
-console.log( typeof temp);
-console.log( typeof user_Name);
-// console.log( typeof username);
-console.log( typeof id);
-console.log( typeof another_Id);
-console.log( typeof big_Number);
-console.log( typeof array);
-// console.log( typeof Objects);
-console.log( typeof my_Funtion);
+// console.log( typeof score);
+// console.log( typeof user_Email);
+// console.log( typeof num);
+// console.log( typeof temp);
+// console.log( typeof user_Name);
+// // console.log( typeof username);
+// console.log( typeof id);
+// console.log( typeof another_Id);
+// console.log( typeof big_Number);
+// console.log( typeof array);
+// // console.log( typeof Objects);
+// console.log( typeof my_Funtion);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory type: 2 types
+// stack(Prmitive), Heap(Non-Primitive)
+
+let stack_Memory_Type = "Azmi-84"
+
+let another_Stack_Memory_Type = stack_Memory_Type
+
+another_Stack_Memory_Type = "Azmi-84_1"
+
+// console.log(stack_Memory_Type);
+// console.log(another_Stack_Memory_Type);
+
+let user_One = {
+    email: "abcd@yahoo.com",
+    password: 123456,
+}
+
+let user_Two = user_One
+
+user_Two.email = "Azmi-84@codespaces.com"
+
+console.log(user_One.email);
+console.log(user_Two.email);
