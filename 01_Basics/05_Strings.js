03588b9070498d26cdb75f1ca34172301771bd8f
Azmi-84 <145701445+Azmi-84@users.noreply.github.com>
2024-07-09 12:52:18 +0000

    string data type explanation

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,30 @@
+let string_Data = "Assalamualikum"
+let number_Data = 34
+
+// let combination = string_Data + number_Data
+// let combination =  number_Data + string_Data 
+
+// console.log(typeof combination);
+// type of data string.
+
+
+// console.log(combination);
+
+// string interpolation 
+// console.log(`${string_Data} ,this is string type, and ${number_Data} ,this is number type data.`);
+
+// string decleration 
+
+const new_String_Data = new String('Assalamualikum, How are you?')
+
+// console.log(new_String_Data);
+
+// console.log(typeof string_Data);
+
+// console.log(typeof new_String_Data);
+
+console.log(new_String_Data[14]);
+console.log(new_String_Data.__proto__);
+
+console.log(new_String_Data.length);
+console.log(new_String_Data.toUpperCase());
