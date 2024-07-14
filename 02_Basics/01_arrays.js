f5e5878402fce95080fe819d2bdb71ecb2358cd0
Azmi-84 <145701445+Azmi-84@users.noreply.github.com>
2024-07-14 02:50:56 +0000

    arrays part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,48 @@
+const my_Array = [1, 2, 3, 4, 5, true, "Azmi-84"]
+// const my_Array_01 = [1, 2, 3, 4, 5, true, "Azmi-84"]
+// const my_Array_02 = new Array (1, 2,  3, 4, 5, true, "Azmi-84")
+
+// console.log(my_Array);
+// console.log(my_Array_01);
+// console.log(my_Array_02);
+
+// console.log(my_Array[5]);
+
+// my_Array.push([1, 2, 3, 4])
+// console.log(my_Array);
+
+// my_Array.pop()
+// console.log(my_Array);
+
+// my_Array.unshift([1, 2, 3, 4, 5])
+// console.log(my_Array);
+
+// my_Array.shift()
+// console.log(my_Array);
+
+// console.log(my_Array.includes(1));
+// console.log(my_Array.includes(10));
+
+// console.log(my_Array.indexOf(1));
+// console.log(my_Array.indexOf(10));
+
+// const my_Array_toString =  my_Array.join()
+
+// console.log(my_Array_toString);
+// console.log(typeof my_Array_toString); // The array has convertef into a string.
+
+// function my_Array_03() {
+//     return (my_Array.slice(1, 3))
+// }
+
+// console.log(my_Array);
+// console.log(my_Array_03());
+// ///////////////////////////////////////////
+// function my_Array_04() {
+//     return (my_Array.splice(1, 3))
+// }
+
+// console.log(my_Array);
+// console.log(my_Array_04());
+// console.log("azmi-84",my_Array);
+
