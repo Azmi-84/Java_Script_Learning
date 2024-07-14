ca460b99c94e701868436ee1f31bcf0337919c7c
Azmi-84 <145701445+Azmi-84@users.noreply.github.com>
2024-07-14 03:23:27 +0000

    arrays part 2

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,11 @@
+const array_01 = new Array (1, 2, 3, 4, 5, true, false, "azmi-84")
+const array_02 =  [1, 2, 3, 4, 5, 6, 7,  true, false, "azmi-84"]
+
+// function array_03() {
+//     return (array_01.push(array_02))
+// }
+
+// console.log(array_03()); // returns the length of the array_03
+
+array_01.push(array_02);
+console.log(array_01);
