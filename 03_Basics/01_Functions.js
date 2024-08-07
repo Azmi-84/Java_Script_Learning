803e612241dcd70bcdeef0b8bd21d466151bd699
Azmi-84 <145701445+Azmi-84@users.noreply.github.com>
2024-08-07 09:07:41 +0000

    Function with objects and arry

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -23,7 +23,33 @@ function my_Array(input) {
 }
 
 // Example usage
-console.log(my_Array([1, 2, 3])); // Outputs: "It is an array"
-console.log(my_Array("not an array")); // Outputs: "Converted it into an array and the array is: ["n","o","t"," ","a","n"," ","a","r","r","a","y"]"
-console.log(my_Array({ name: "azmi-84", age: 25 })); // Outputs: "Converted it into an array and the array is: ["azmi-84", 25]"
-console.log(my_Array(42)); // Outputs: "Converted it into an array and the array is: [42]"
+// console.log(my_Array([1, 2, 3])); // Outputs: "It is an array"
+// console.log(my_Array("not an array")); // Outputs: "Converted it into an array and the array is: ["n","o","t"," ","a","n"," ","a","r","r","a","y"]"
+// console.log(my_Array({ name: "azmi-84", age: 25 })); // Outputs: "Converted it into an array and the array is: ["azmi-84", 25]"
+// console.log(my_Array(42)); // Outputs: "Converted it into an array and the array is: [42]"
+
+//  for adding mutiple values, the result is given in a single array
+
+function addinginfinitevalue(...values) {
+    return values.reduce((sum, currentValue) => sum + currentValue, 0);
+}
+
+// console.log(addinginfinitevalue(20, 40, 60, 80, 100, 120)); // Output: 420
+// 5.08 =>20
+
+const user = {
+    username: "azmi",
+    ID: 84,
+    email: "azmi_84@github.com"
+}
+function handleObject(anyObject) {
+    // console.log(`Username is ${anyObject.username} and ID is ${anyObject.ID} and email is ${anyObject.email}`);
+}
+handleObject(user)
+
+const array_00 = [123, 456, 789, 120]
+
+function returnArray(array_00){
+    return array_00[2]
+}
+console.log(returnArray(array_00));
