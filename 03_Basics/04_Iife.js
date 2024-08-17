9ca876b4d6fc367670243194e45ce966123a3e87
Azmi-84 <145701445+Azmi-84@users.noreply.github.com>
2024-08-17 04:14:50 +0000

    chore: Add IIFE code file

---
+++ 03_Basics/04_Iife.js
@@ -0,0 +1,6 @@
+//  Immediately Invoked Function Expression (IIFE)
+//  to avoid polluting the global scope with variables we can use IIFE to create a new scope for our variables
+
+(function() {
+  console.log('IIFE');
+})();
