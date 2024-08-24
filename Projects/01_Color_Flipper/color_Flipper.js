33f10925d4c7d86690db9bbd6116977adc4d090f
Azmi-84 <azmi@iut-dhaka.edu>
2024-08-24 15:34:24 +0000

    chore: Remove unused files and optimize code for color flipper and button projects

--- Projects/01_Color_Flipper/color_Flipper.js
+++
@@ -1,1 +0,0 @@
-const colors = ["green", "red", "rgba(255, 128, 214)", "#f1f5f8"];
--- Projects/02_button/02_button.js
+++
@@ -1,7 +0,0 @@
-function on_Click_Button(){
-    alert("saved");
-}
-
-const button = document.querySelector("button");
-
-button.addEventListener('click', on_Click_Button);
